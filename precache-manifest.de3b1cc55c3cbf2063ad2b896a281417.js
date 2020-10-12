5089fc4780c39ff406d5f639873fd178c463433c
MarcosVn <marcos.vnaraujo@gmail.com>
2020-10-12 19:58:34 -0300

    Updates

---
+++ precache-manifest.de3b1cc55c3cbf2063ad2b896a281417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fa32e1d0718cdeb7b744c40a604944",
+    "url": "/react-task-list/index.html"
+  },
+  {
+    "revision": "92a6ab3bbbad8d09b3f1",
+    "url": "/react-task-list/static/css/main.1a7328cd.chunk.css"
+  },
+  {
+    "revision": "e62b4e11908906f792a3",
+    "url": "/react-task-list/static/js/2.fae88ce0.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/react-task-list/static/js/2.fae88ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a6ab3bbbad8d09b3f1",
+    "url": "/react-task-list/static/js/main.25af1e40.chunk.js"
+  },
+  {
+    "revision": "0c9a628eecd2373f4110",
+    "url": "/react-task-list/static/js/runtime-main.8ac737a7.js"
+  }
+]);
