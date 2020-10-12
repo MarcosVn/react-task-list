f4aa88fb32662b413d258d44e342a253a3388f9f
MarcosVn <marcos.vnaraujo@gmail.com>
2020-10-12 19:33:02 -0300

    Updates

---
+++ precache-manifest.c722d166c175f5d5cd8a2141fbe6be2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09705e85afc73f1b734a4581b7f858d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1437370e61b17241cee2",
+    "url": "./static/css/main.da21538d.chunk.css"
+  },
+  {
+    "revision": "e62b4e11908906f792a3",
+    "url": "./static/js/2.fae88ce0.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "./static/js/2.fae88ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1437370e61b17241cee2",
+    "url": "./static/js/main.1a7c6cec.chunk.js"
+  },
+  {
+    "revision": "e175015c001fa49cc687",
+    "url": "./static/js/runtime-main.ab7bc8e7.js"
+  }
+]);
